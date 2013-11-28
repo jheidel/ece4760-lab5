11872e4c88aab4698d692a57b1500a6554881b80
Jeff Heidel <jeh295@cornell.edu>
2013-11-27 21:05:35 -0500

    Add logging interface

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import sys
+import sys, logging, signal
 from gi.repository import Gtk, Gdk, GLib
 
 from pylib.ildaparser import IldaParser
 from pylib.laserviz import LaserViz
-
-
-p = IldaParser(sys.argv[1])
-
+from pylib.log import init_logging, with_logging
 
 from threading import Thread, Event
 from time import sleep
+
+
+p = IldaParser(sys.argv[1])
 
 class FramePlayer(Thread):
     def __init__(self, lv, p):
@@ -31,12 +31,13 @@ class FramePlayer(Thread):
                 if (self.kill.is_set()):
                     return
 
-
+@with_logging
 class MainGUI:
 
     def window_destroy(self, event):
         Gtk.main_quit()
         self.player.stop()
+        self.log.info("Bye!")
 
     def __init__(self):
         self.gladefile = "pylib/gui.glade"
@@ -52,10 +53,23 @@ class MainGUI:
 
         #self.laserviz.set_frame(p.frames[0])
         self.player = FramePlayer(self.laserviz, p)
-        
 
+        #Ctrl+C handling
+        def handler(signum, frame):
+            self.log.warning("INTERRUPT; shutting down...")
+            self.window_destroy(None)
+        signal.signal(signal.SIGTERM, handler)
+        signal.signal(signal.SIGINT, handler)
+
+        #This fairly pointless function is necessary to periodically wake up
+        #the gtk main thread to detect system interrupts even when not focused
+        #I believe this is due to a Gtk bug
+        GLib.timeout_add(500, lambda : True)
+        
         Gdk.threads_init()
         GLib.threads_init()
+
+        self.log.info("ECE 4760 Laser Controller Starting...")
 
         Gdk.threads_enter()
         Gtk.main()
@@ -63,4 +77,5 @@ class MainGUI:
 
 
 if __name__ == "__main__":
+    init_logging(log_level = logging.DEBUG)
     main = MainGUI()
---
+++ pylib/logger_formatting.py
@@ -0,0 +1,46 @@
+import logging
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#The background is set with 40 plus the number of the color, and the foreground with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': MAGENTA,
+    'INFO': GREEN,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED,
+}
+
+#Adds custom log levels to the input logger
+def add_custom_logging(logger):
+    def logging_factory(logger, debug_level):
+        def custom_debug(msg, *args, **kwargs):
+            if logger.level >= debug_level:
+               return
+            logger._log(debug_level, msg, args, kwargs)
+        return custom_debug 
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, date, use_color = True):
+        logging.Formatter.__init__(self, msg, date)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
